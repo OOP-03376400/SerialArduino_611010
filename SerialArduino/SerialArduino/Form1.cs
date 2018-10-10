20967e89e3a6e25e9305394478838c3a43a6c365
Koson Trachu <koson.trachu@gmail.com>
2018-10-10 10:55:32 +0700

    data received from serial port

--- SerialArduino/SerialArduino/Form1.cs
+++ SerialArduino/SerialArduino/Form1.cs
@@ -21,8 +21,58 @@ namespace SerialArduino
         private void button1_Click(object sender, EventArgs e)
         {
             string[] ports = SerialPort.GetPortNames();
+            listBox1.Items.Clear();
             listBox1.Items.AddRange(ports);
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(ports);
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen == true)
+            {
+                return;
+            }
+            else
+            {
+                serialPort1.Open();
+                button2.Enabled = false;
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                return;
+            }
+            else
+            {
+                serialPort1.Close();
+                button2.Enabled = true;
+                button3.Enabled = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+            Console.WriteLine(serialPort1.PortName);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate =  Int32.Parse(comboBox2.Text);
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string str = sp.ReadExisting();
+            Console.WriteLine(str);
         }
     }
 }
