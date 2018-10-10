e0fb06f940bfc11f8b4c2a6e2c6c2fa8213208ba
koson trachu <koson.trachu@gmail.com>
2018-10-10 20:20:56 +0700

    reformat sinewave output

--- arduino_project/sine_wave/sine_generate.ino/sine_generate.ino.ino
+++ arduino_project/sine_wave/sine_generate.ino/sine_generate.ino.ino
@@ -1,25 +1,19 @@
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
-*/
-
 // the setup routine runs once when you press reset:
+int i;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  i = 0;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   float something = millis()/1000.0;
   int value = 128.0 + 128 * sin( something * 2.0 * PI  );
+ 
+  Serial.print(i);
+  Serial.print(", ");
   Serial.println(value);
-  delay(200);        // delay in between reads for stability
+  i++;
+  delay(50);        // delay in between reads for stability
 }
